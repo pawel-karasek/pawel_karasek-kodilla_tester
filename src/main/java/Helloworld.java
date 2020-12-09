b7ec8c5faa47eda3400c608bf335b5f61b5c852b
Pawel Karasek <karasek.pawel@gmail.com>
2020-12-09 12:22:28 +0100

    Helloworld

---
+++ src/main/java/Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld
+    {public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Pawel");
+        System.out.println(34);
+        System.out.println('K');
+}
+}
