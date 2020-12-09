88e600aa423854f1a6ecdaec2b20add43037cf5f
Pawel Karasek <karasek.pawel@gmail.com>
2020-12-09 13:42:15 +0100

    MainExtended

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String imieInazwisko = "Pawel Karasek";
+        double pi = 3.14;
+        int phoneNumber = 662889339;
+        boolean smart = true;
+        char superMan = 'S';
+        String mail = "karasek.pawel@gmail.com";
+            System.out.println(imieInazwisko);
+            System.out.println(pi);
+            System.out.println(phoneNumber);
+            System.out.println(smart);
+            System.out.println(superMan);
+            System.out.println(mail);
+    }
+}
