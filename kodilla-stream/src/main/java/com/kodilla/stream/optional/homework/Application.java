459bdafa382e1020c7605af6667342b418994934
Pawel Karasek <karasek.pawel@gmail.com>
2021-02-21 13:25:19 +0100

    zadania 6.1, 6.2

--- kodilla-stream/src/main/java/com/kodilla/stream/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/optional/homework/Application.java
@@ -2,11 +2,21 @@ package com.kodilla.stream.optional.homework;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Application {
     public static void main(String[] args) {
 
         List<Student> studentList = new ArrayList<>();
-        studentList.add(new Student("Paweł", ""));
+        studentList.add(new Student("Paweł", new Teacher("Wojtek")));
+        studentList.add(new Student("Piotr", new Teacher("Adam")));
+        studentList.add(new Student("Robert", null));
+
+        for (Student students : studentList
+             ) {
+            System.out.println(studentList);
+        }
+        Optional<Student> optionalStudent = Optional.ofNullable();
+
     }
 }
