e30170fa09967d4c71f67db71b474f8ca9709844
Pawel Karasek <karasek.pawel@gmail.com>
2021-02-28 14:53:04 +0100

    zadanie 7.2

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -0,0 +1,36 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class GamblingMachineTestSuite {
+    GamblingMachine gamblingMachine = new GamblingMachine();
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/userNumbers-happyPath.csv")
+    public void shouldReturnHitedNumbers(Set<Integer> userNumbers) throws InvalidNumbersException{
+        gamblingMachine.howManyWins(userNumbers);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/userNumbers-fourNumbers.csv")
+    public void shouldThrowExceptionFourNumbers(Set<Integer> userNumbers) throws InvalidNumbersException{
+        InvalidNumbersException exception = assertThrows(InvalidNumbersException.class,
+                () -> gamblingMachine.howManyWins(userNumbers));
+        assertEquals("Wrong numbers provided.", exception);
+
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/userNumbers-numbersOutOfRange.csv")
+    public void shouldThrowExceptionOutOfRange(Set<Integer> userNumbers) throws InvalidNumbersException{
+        InvalidNumbersException exception = assertThrows(InvalidNumbersException.class,
+                () -> gamblingMachine.howManyWins(userNumbers));
+        assertEquals("Wrong numbers provided.", exception);
+    }
+}
