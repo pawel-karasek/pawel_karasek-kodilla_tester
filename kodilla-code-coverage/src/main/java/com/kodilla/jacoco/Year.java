ff4924e19eb1ef80d28054e86b27e0624debd1ea
Pawel Karasek <karasek.pawel@gmail.com>
2021-04-25 14:27:07 +0200

    zadanie 15.2

--- kodilla-code-coverage/src/main/java/com/kodilla/jacoco/Year.java
+++ kodilla-code-coverage/src/main/java/com/kodilla/jacoco/Year.java
@@ -1,20 +1,16 @@
 package com.kodilla.jacoco;
 
 public class Year {
-    int year;
+    int testYear;
 
     public Year(int year) {
-        this.year = year;
+        this.testYear = year;
     }
 
     public boolean isLeap() {
-        if (this.year % 4 == 0) {
-            if (this.year % 100 == 0) {
-                if (this.year % 400 == 0) {
-                    return true;
-                } else {
-                    return false;
-                }
+        if (this.testYear % 4 == 0) {
+            if (this.testYear % 100 == 0) {
+                return (this.testYear % 400 == 0);
             } else {
                 return true;
             }
--- kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class YearTest {
 
     @Test
-    public void shouldBeLeapYearIfDivisibleBy400() {
+    void shouldBeLeapYearIfDivisibleBy400() {
 
         //given
         Year year = new Year(1600);
@@ -19,4 +19,42 @@ class YearTest {
         assertTrue(isLeapYear);
     }
 
+    @Test
+    void shouldNotBeLeapYearIfNotDivisibleBy400() {
+
+        //given
+        Year year = new Year(1601);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertEquals(false, isLeapYear);
+    }
+
+    @Test
+    void shouldBeLeapYearIfDivisibleBy4AndNotBy100() {
+
+        //given
+        Year year = new Year(1604);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+    void shouldNotBeLeapYearIfDivisibleBy100AndNotBy4() {
+
+        //given
+        Year year = new Year(1700);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertEquals(false, isLeapYear);
+    }
 }
